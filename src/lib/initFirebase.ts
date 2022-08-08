3e424d29bef4ae7d8fa6d6060b61e4b32ae4e51b
Adrian-stuff <68731858+Adrian-stuff@users.noreply.github.com>
2022-08-08 18:36:19 +0800

    firebase stuff

--- src/lib/initFirebase.ts
+++ src/lib/initFirebase.ts
@@ -1,6 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore/lite";
+import { getStorage } from "firebase/storage";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -20,3 +22,6 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+const db = getFirestore(app);
+const storage = getStorage(app);
+export { db, analytics, storage };
